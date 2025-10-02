78948a1e3719779e2f006cb522bd47a05d183e17
Hunter Hardy <hunter@tmbr.dog>
2025-10-02 17:53:31 +1000

    feat: player class

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import pygame
 
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    print(player)
 
-    while True:
+    while True:  # ma beautiful loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+
         screen.fill("black")
-        
-        pygame.display.flip() # call last
+        dt = clock.tick(60) / 1000
+        player.draw(screen)
+        pygame.display.flip()  # call last !!
 
 
 if __name__ == "__main__":
